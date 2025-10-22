5fc189a7da5e2033f37a7a29c96eb677eeafb7f2
SamaWyna <etornamasamoah@gmail.com>
2025-10-22 02:58:01 +0100

    Connect artist auth flows to backend APIs

--- zamio_frontend/src/App.tsx
+++ zamio_frontend/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@zamio/ui';
 import router from './lib/router';
+import { AuthProvider } from './lib/auth';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
