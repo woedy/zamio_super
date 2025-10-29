58b45f14b807bc0ddff7569a70ff4d1f15b696c8
SamaWyna <etornamasamoah@gmail.com>
2025-10-29 14:00:34 +0000

    Serve static and media assets from Django

--- zamio_backend/core/asgi.py
+++ zamio_backend/core/asgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from django.conf import settings
 from django.core.asgi import get_asgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = WhiteNoise(
+    django_asgi_app,
+    root=str(settings.STATIC_ROOT) if settings.STATIC_ROOT else None,
+    autorefresh=settings.DEBUG,
+)
--- zamio_backend/core/wsgi.py
+++ zamio_backend/core/wsgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+django_wsgi_app = get_wsgi_application()
+
+application = WhiteNoise(
+    django_wsgi_app,
+    root=str(settings.STATIC_ROOT) if settings.STATIC_ROOT else None,
+    autorefresh=settings.DEBUG,
+)
